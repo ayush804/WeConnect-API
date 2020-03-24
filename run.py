8c8f38b1cc32966be73e6beeaf9bca561986c545
Ayush <ayush804@gmail.com>
2020-03-24 12:04:12 +0530

    commit

--- run.py
+++ run.py
@@ -15,5 +15,5 @@ def create_app(config_filename):
 
 
 if __name__ == "__main__":
-    app = create_app("config")
-    app.run(debug=True)
+    application = create_app("config")
+    application.run(debug=True)
