b1cbdebad4ef8125d8c3924946f10b6717525369
Ayush <ayush804@gmail.com>
2020-03-24 13:02:00 +0530

    commit

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
 from flask import Flask, Blueprint
+from app import api_bp
+from model import db
 
 application = Flask(__name__)
-#def create_app(config_filename):
 application.config.from_object("config")
-from app import api_bp
 application.register_blueprint(api_bp, url_prefix='/api')
-
-from model import db
 db.init_app(application)
 
-    #return application
+
+def create_app(config_filename):
+    global application
+    return application
 
 
 if __name__ == "__main__":
-    #application = create_app("config")
     application.run(debug=True)
