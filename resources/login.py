bc173ed358f38ab1f92166a7bf3762e2e1df46d6
Ayush <ayush804@gmail.com>
2020-03-25 10:21:19 +0530

    added more response

--- resources/login.py
+++ resources/login.py
@@ -2,10 +2,12 @@ from flask_jwt_extended import create_access_token
 from flask_restful import Resource
 from flask import request
 from model import db, WeConnect_Users
+import re
 
 
 class Login(Resource):
     def post(self):
+        regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
         try:
             json_data = request.get_json(force=True)
             if not json_data:
@@ -18,10 +20,19 @@ class Login(Resource):
                 password = json_data['password']
             else:
                 return {"message": "Missing Password"}
-            if db.session().query(WeConnect_Users).filter_by(emailId=emailId, password=password).first():
-                token = create_access_token(identity=emailId)
-                return {"message" : "Authenticated successfully", "token": token}, 200
+            if re.search(regex, emailId):
+                if db.session().query(WeConnect_Users).filter_by(emailId=emailId).first():
+                    user = db.session().query(WeConnect_Users).filter_by(emailId=emailId).first()
+                    if user.__dict__["password"] == password:
+                        token = create_access_token(identity=emailId)
+                        return {"message" : "Authenticated successfully", "Token": token,
+                                "Name": user.__dict__["name"], "DateofBirth" : user.__dict__["dob"],
+                                "Sex" : user.__dict__["sex"]}, 200
+                    else:
+                        return {"message" : "Incorrect Password"}
+                else:
+                    return {"message" : "Email id not registered"}, 401
             else:
-                return {"message" : "Invalid Username or Password"}, 401
+                return {"message" : "Email id is not in valid format"}, 422
         except:
             return {"message" : "Something went wrong"}, 500
--- resources/register.py
+++ resources/register.py
@@ -1,10 +1,11 @@
 from flask_restful import Resource
 from flask import request
 from model import db, WeConnect_Users
-
+import re
 
 class Register(Resource):
     def post(self):
+        regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
         try:
             json_data = request.get_json(force=True)
             sex, name="", ""
@@ -15,8 +16,6 @@ class Register(Resource):
                 emailId = json_data['emailId']
             else:
                 return {"message" : "Missing Email id"}, 422
-            if db.session().query(WeConnect_Users).filter_by(emailId=emailId).first():
-                return {"message" : "User already exist"}, 409
             if "password" in json_data:
                 password = json_data['password']
             else:
@@ -27,11 +26,16 @@ class Register(Resource):
                 dob = json_data['dob']
             if "name" in json_data:
                 name = json_data['name']
-            db.create_all()
-            db.session.commit()
-            user = WeConnect_Users(emailId, password, sex, dob, name)
-            db.session.add(user)
-            db.session.commit()
-            return {"message": "User Created"}, 201
+            if re.search(regex, emailId):
+                if db.session().query(WeConnect_Users).filter_by(emailId=emailId).first():
+                    return {"message" : "User already exist"}, 409
+                db.create_all()
+                db.session.commit()
+                user = WeConnect_Users(emailId, password, sex, dob, name)
+                db.session.add(user)
+                db.session.commit()
+                return {"message": "User Created"}, 201
+            else:
+                return {"message" : "Email id is not in valid format"}
         except:
             return {"message" : "Something went wrong"}, 500
